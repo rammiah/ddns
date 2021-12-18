77ba7a668a4c23cfedee8ebf7a915ae9cbfbddec
rammiah <rammiahcn@gmail.com>
2021-12-18 16:41:42 +0800

    use libdns

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module ddns
 go 1.15
 
 require (
-	github.com/aliyun/alibaba-cloud-sdk-go v1.61.1139
 	github.com/joho/godotenv v1.3.0
+	github.com/libdns/alidns v1.0.2
+	github.com/libdns/libdns v0.2.1
 )
--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"errors"
 	"io"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
-	"strings"
+	"time"
 
-	"github.com/aliyun/alibaba-cloud-sdk-go/services/alidns"
 	"github.com/joho/godotenv"
+	"github.com/libdns/alidns"
+	"github.com/libdns/libdns"
+)
+
+const (
+	DnsTypeAAAA = "AAAA"
 )
 
 var (
-	dnsCli *alidns.Client
+	AliPrd *alidns.Provider
 )
 
 func init() {
@@ -29,13 +34,10 @@ func init() {
 		log.Printf("load .env error: %v\n", err)
 		panic(err)
 	}
-
-	cli, err := alidns.NewClientWithAccessKey("cn-beijing", os.Getenv("ACCESSKEY_ID"), os.Getenv("ACCESSKEY_SECRET"))
-	if err != nil {
-		log.Printf("create client error: %v\n", err)
-		panic(err)
+	AliPrd = &alidns.Provider{
+		AccKeyID:     os.Getenv("ACCESSKEY_ID"),
+		AccKeySecret: os.Getenv("ACCESSKEY_SECRET"),
 	}
-	dnsCli = cli
 }
 
 const (
@@ -73,77 +75,32 @@ func GetIPv6IP() net.IP {
 	return ip
 }
 
-func QueryRecordID(name string) (id, value string, _ error) {
-	req := alidns.CreateDescribeSubDomainRecordsRequest()
-	req.Scheme = "https"
-
-	req.SubDomain = name
-
-	resp, err := dnsCli.DescribeSubDomainRecords(req)
-	if err != nil {
-		log.Printf("query sub domain records error: %v\n", err)
-		return "", "", err
-	}
-	for _, rec := range resp.DomainRecords.Record {
-		if rec.Type == "AAAA" {
-			log.Printf("query record success: %v, ip %v\n", rec.RecordId, rec.Value)
-			return rec.RecordId, rec.Value, nil
-		}
-	}
-
-	return "", "", errors.New("no results")
-}
-
-func UpdateDdns(name string, ip net.IP) error {
-	recId, val, err := QueryRecordID(name)
-	if err != nil {
-		log.Printf("query record id error: %v\n", err)
-		return err
-	}
-	log.Printf("query name %v success, ip %v, record id %v\n", name, val, recId)
-
-	if val == ip.String() {
-		log.Printf("ip is same, input[%v], exists[%v], return success\n", ip.String(), val)
-		return nil
-	}
-
-	req := alidns.CreateUpdateDomainRecordRequest()
-	req.Scheme = "https"
-	req.RR = name[:strings.Index(name, ".")]
-	req.RecordId = recId
-	req.Type = "AAAA"
-	req.Value = ip.String()
-
-	resp, err := dnsCli.UpdateDomainRecord(req)
-	if err != nil {
-		log.Printf("update record error: %v\n", err)
-		return err
-	}
-
-	log.Printf("request response: success %v, request id %v\n", resp.IsSuccess(), resp.RequestId)
-
-	return nil
-}
-
-func RunUpdate() {
+func RunUpdate(sub, dom, typ string) {
 	ip := GetIPv6IP()
 	if ip == nil {
 		log.Printf("get ip failed\n")
 		return
 	}
-	log.Printf("query ip success: %v\n", ip)
+	log.Printf("get ip success: %v\n", ip)
 
-	name := "laptop.awsl.xin"
+	ctx := context.Background()
+	_, err := AliPrd.SetRecords(ctx, dom, []libdns.Record{
+		{
+			Type:  typ,
+			Name:  sub,
+			Value: ip.String(),
+			TTL:   10 * time.Minute,
+		},
+	})
 
-	log.Printf("get ip success: %v", ip)
-	if err := UpdateDdns(name, ip); err != nil {
-		log.Printf("update ip error: %v\n", err)
+	if err != nil {
+		log.Printf("update records error: %v\n", err)
 		return
 	}
-
-	log.Printf("update success")
+	log.Printf("update %v.%v success\n", sub, dom)
 }
 
 func main() {
-	RunUpdate()
+	RunUpdate("laptop", "awsl.xin", DnsTypeAAAA)
+	RunUpdate("qbit", "awsl.xin", DnsTypeAAAA)
 }
